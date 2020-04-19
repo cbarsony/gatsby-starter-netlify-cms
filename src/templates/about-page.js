6c5bd386ae9e7c8fcafe3f3d7797b9e37e0ae9e1
CsabaBarsony <csaba.barsony@gmail.com>
2020-04-19 11:42:06 +0200

    first commit

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -15,6 +15,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
             <div className="section">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
+                Majom vagy
               </h2>
               <PageContent className="content" content={content} />
             </div>
